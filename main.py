d3c4d76a05c550f2357c1b8913d64a64756b28ea
Azamat <azamathasanov.n@gmail.com>
2026-09-07 00:39:50 +0400

    todo v.01

---
+++ main.py
@@ -0,0 +1,28 @@
+add_list=[]
+count=0
+def todo():
+    global add_list
+    global count
+   
+    while True:
+        print("=== TODO LIST ===")
+        print("1. Tapşırıq əlavə et\n2. Tapşırıqları göstər\n3. Tapşırıq sil\n4. Çıxış")
+        dyr=input("isdediyiniz reqemi girin")
+        if dyr=="1":  
+            tsk_write=input("tapsiriqin adini girin")
+            add_list.append(tsk_write)
+            count+=1
+        elif dyr=="2":
+            for i,item in enumerate(add_list,start=1):
+                print(f"{i}. {item}")
+        elif dyr=="3":
+            for c in range(len(add_list)):
+                dyrss=int(input("reqemi girin"))
+                if c==dyrss:
+                    add_list.pop(dyrss)
+        elif dyr=="4":
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n✅sistemden cixis edildi Tesekkur")
+            break
+        else:
+            print("1 ile 4 arasinda reqem secin") 
+todo()
